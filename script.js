b7f4c050b2523941f1d20a3643a73d9679b43509
Alina Ali01~ <alina.zenam@hotmail.com>
2022-12-12 19:20:02 +0000

    made function for clock, made questions come on page and started forloop for choices

--- script.js
+++ script.js
@@ -1,24 +1,24 @@
 // create questions 
 var questions = [
+    // {
+    //     question: '', 
+    //     answer: '',
+    //     choices: [],
+    // },
+    // {
+    //     question: '', 
+    //     answer: '',
+    //     choices: [],
+    // },
+    // {
+    //     question: '', 
+    //     answer: '',
+    //     choices: [],
+    // },
     {
-        question: '', 
-        answer: '',
-        choices: [],
-    },
-    {
-        question: '', 
-        answer: '',
-        choices: [],
-    },
-    {
-        question: '', 
-        answer: '',
-        choices: [],
-    },
-    {
-        question: '', 
-        answer: '',
-        choices: [],
+        question: 'abc', 
+        answer: 'abc',
+        choices: ['abc', 'a', 'b', 'c'],
     }
 ]
 
@@ -34,8 +34,14 @@ var questionsEL = document.getElementById (
     'questions'
 )
 var timer; 
-
-
+var time = questions.length * 15;
+var timeEL = document.getElementById (
+    'time'
+)
+var qIndex = 0
+var choicesEL = document.getElementById (
+    'choices'
+)
 // create event listener to start quiz
 startButton.addEventListener ('click', startQuiz);
 // clear the page of the intro, unhide the questions and start the timer
@@ -43,7 +49,28 @@ function startQuiz () {
     startScreen.classList.add("hide")
     questionsEL.removeAttribute("class")
     timer = setInterval(clock, 1000)
+    timeEL.textContent=time;
+
     getQuestions()
 }
 
 // make function for clock
+function clock () {
+    time--;
+timeEL.textContent=time;
+}
+// make questions come on the page
+function getQuestions () {
+var currentQ = questions[qIndex]
+var questionTitle = document.getElementById (
+    'question-title'
+)
+questionTitle.textContent = currentQ.question
+choicesEL.innerHTML = '';
+// create a forloop to go through the choices and create an element button and append to the choicesEL area
+for(var i = 0; x < buttonsWanted; i++){
+    var button = doc.createElement('button');
+    button.setAttribute('text', 'yourtext');
+    docFrag.appendChild(button);
+  }
+}
