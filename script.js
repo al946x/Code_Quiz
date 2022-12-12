f116486c0f12e36e0932c738c0a7243f48be68aa
Alina Ali01~ <alina.zenam@hotmail.com>
2022-12-12 18:50:25 +0000

    finished start quiz and created question template

--- script.js
+++ script.js
@@ -1,4 +1,26 @@
 // create questions 
+var questions = [
+    {
+        question: '', 
+        answer: '',
+        choices: [],
+    },
+    {
+        question: '', 
+        answer: '',
+        choices: [],
+    },
+    {
+        question: '', 
+        answer: '',
+        choices: [],
+    },
+    {
+        question: '', 
+        answer: '',
+        choices: [],
+    }
+]
 
 
 // identify html for global variables
@@ -8,11 +30,20 @@ var startButton = document.getElementById (
 var startScreen = document.getElementById (
     'start-screen'
 )
+var questionsEL = document.getElementById (
+    'questions'
+)
+var timer; 
+
 
 // create event listener to start quiz
 startButton.addEventListener ('click', startQuiz);
 // clear the page of the intro, unhide the questions and start the timer
 function startQuiz () {
     startScreen.classList.add("hide")
+    questionsEL.removeAttribute("class")
+    timer = setInterval(clock, 1000)
+    getQuestions()
 }
+
 // make function for clock
